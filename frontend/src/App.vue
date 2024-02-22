7d13c7b0c826290a391b3a09ce5d2f7ca467bb73
Szzng <seungheeseo53@gmail.com>
2024-02-23 02:42:22 +0900

    feat: Home 완성

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,36 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import TheFooter from '@/components/TheFooter.vue'
+import TheNavbar from '@/components/TheNavbar.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="App">
+    <nav>
+      <TheNavbar />
+    </nav>
+    <main class="dark:bg-gray-900 pt-16">
+      <router-view />
+    </main>
+    <footer>
+      <TheFooter />
+    </footer>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+footer {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  z-index: 10;
 }
 </style>
