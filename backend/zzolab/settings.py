c666defb23ca25623870d1f78c188ca173ee88b4
Szzng <seungheeseo53@gmail.com>
2024-03-16 21:20:15 +0900

    chore:settings_ CSRF_TRUSTED_ORIGINS

--- backend/zzolab/settings.py
+++ backend/zzolab/settings.py
@@ -32,6 +32,8 @@ CSRF_COOKIE_SECURE = env.bool('CSRF_COOKIE_SECURE', default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
+
 # CORS
 CORS_ALLOWED_ORIGINS = env.list(
     "CORS_ALLOWED_ORIGINS", default=["http://127.0.0.1:5173", "http://localhost:5173"]
