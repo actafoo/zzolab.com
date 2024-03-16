03968d7e0584c3fb92f1e520d6d082ef102f4631
Szzng <seungheeseo53@gmail.com>
2024-03-16 21:04:34 +0900

    chore: settings_ CSRF_COOKIE_SECURE 추가

--- backend/zzolab/settings.py
+++ backend/zzolab/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
+CSRF_COOKIE_SECURE = env.bool('CSRF_COOKIE_SECURE', default=False)
+
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
 # CORS
